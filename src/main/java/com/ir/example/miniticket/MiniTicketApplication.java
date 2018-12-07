8fca1ee4e38aad4c46871e428992cdd3c6c7b4eb
Thiago Souza Ferreira <thiago.ferreira@ingressorapido.com.br>
2018-12-07 15:26:19 -0200

    first commit

---
+++ src/main/java/com/ir/example/miniticket/MiniTicketApplication.java
@@ -0,0 +1,12 @@
+package com.ir.example.miniticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTicketApplication.class, args);
+	}
+}
